734a80215e55a42728e06f6eca2fa07b9998c5cb
AkshitBytes <akshitKumar129@gmail.com>
2026-02-14 22:14:48 +0530

    initialized the frontend in react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Signup from './components/Auth/singup'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Signup />
+    </>
+     )
+}
+
+export default App
---
+++ src/components/Auth/singup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function singup() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default singup
